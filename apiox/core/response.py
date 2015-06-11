7e742bebcc23e79d24b467ffe7c2b3d531486f77
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2015-06-11 09:36:50 +0100

    Sort dict keys when serializing JSON responses

--- apiox/core/response.py
+++ apiox/core/response.py
@@ -3,7 +3,7 @@ import json
 from aiohttp.web import Response
 
 def JSONResponse(*, body=None, content_type=None, base=Response, **kwargs):
-    body = json.dumps(body, indent=2).encode()
+    body = json.dumps(body, indent=2, sort_keys=True).encode()
     if content_type is None:
         content_type = 'application/json'
     return base(body=body, content_type=content_type, **kwargs)
