71b973db8c286eb97dfd376926c45e2b83fadc61
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2015-09-21 14:38:56 +0100

    Make a factory for basic auth; don't encourage use of partial()

--- apiox/core/middleware/__init__.py
+++ apiox/core/middleware/__init__.py
@@ -2,7 +2,7 @@
 from .basic import basic_auth_middleware
 from .negotiate import negotiate_auth_middleware, add_negotiate_token
 from .oauth2 import oauth2_middleware
-from .remote_user import remote_user_middleware, persist_remote_user_query_param
+from .remote_user import remote_user_middleware_factory, persist_remote_user_query_param
 
 # Exception reporting
 from .raven import raven_middleware
--- apiox/core/middleware/remote_user.py
+++ apiox/core/middleware/remote_user.py
@@ -4,24 +4,24 @@ from urllib.parse import urlparse, parse_qs, urlencode
 
 from ..db import Principal
 
-@asyncio.coroutine
-def remote_user_middleware(app, handler,
-                           use_header=False, use_param=False,
-                           accept_from=()):
+def remote_user_middleware_factory(use_header=False, use_param=False,
+                                   accept_from=()):
     @asyncio.coroutine
-    def middleware(request):
-        remote_addr = ipaddress.ip_address(request.transport.get_extra_info('peername')[0])
-        if any(remote_addr in range for range in accept_from):
-            if use_header and 'X-Remote-User' in request.headers:
-                principal = yield from Principal.lookup(app, request.headers['X-Remote-User'])
-                if principal:
-                    request.token = yield from principal.get_token_as_self()
-            if use_param and 'remote_user' in request.GET:
-                principal = yield from Principal.lookup(app, request.GET['remote_user'])
-                if principal:
-                    request.token = principal.get_token_as_self()
-        return (yield from handler(request))
-    return middleware
+    def remote_user_middleware(app, handler):
+        @asyncio.coroutine
+        def middleware(request):
+            remote_addr = ipaddress.ip_address(request.transport.get_extra_info('peername')[0])
+            if any(remote_addr in range for range in accept_from):
+                if use_header and 'X-Remote-User' in request.headers:
+                    principal = yield from Principal.lookup(app, request.headers['X-Remote-User'])
+                    if principal:
+                        request.token = yield from principal.get_token_as_self()
+                if use_param and 'remote_user' in request.GET:
+                    principal = yield from Principal.lookup(app, request.GET['remote_user'])
+                    if principal:
+                        request.token = principal.get_token_as_self()
+            return (yield from handler(request))
+        return middleware
 
 def persist_remote_user_query_param(request, response):
     if 'remote_user' in request.GET and 'Location' in response.headers:
