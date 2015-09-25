96148a2f815a07ebad785da4e3cd218d87e4a716
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2015-09-25 14:50:54 +0100

    Work around iOS bug that hides 401 challenges in some circumstances

--- apiox/core/middleware/cors.py
+++ apiox/core/middleware/cors.py
@@ -19,3 +19,15 @@ def add_cors_headers(request, response):
         if 'Access-Control-Request-Headers' in request.headers:
             response.headers['Access-Control-Allow-Headers'] = request.headers['Access-Control-Request-Headers']
         response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+
+    # There's a bug in iOS that means that 401 responses are hidden if there's a Basic challenge in the
+    # WWW-Authenticate header. This lets clients request that the header be renamed so as not to trigger it.
+    # See e.g. http://stackoverflow.com/questions/11025213/ios-authentication-using-xmlhttprequest-handling-401-reponse
+    # for more details.
+    if 'X-Rename-WWW-Authenticate' in request.headers and \
+                    'WWW-Authenticate' in response.headers and \
+                    request.headers['X-Rename-WWW-Authenticate'] not in response.headers:
+        for key, value in response.headers.items():
+            if key.upper() == 'WWW-Authenticate'.upper():
+                response.headers.add(request.headers['X-Rename-WWW-Authenticate'], value)
+        del response.headers['WWW-Authenticate']
